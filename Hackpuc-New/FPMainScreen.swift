582107afe1ddeda01da610c660461351d768de39
TauanBinato <tauanflores@gmail.com>
2015-12-12 11:36:09 -0200

    inicio FPMainScreen

---
+++ Hackpuc-New/FPMainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FPMainScreen.swift
+//  Hackpuc-New
+//
+//  Created by Tauan Flores on 12/12/15.
+//  Copyright © 2015 Grupo13. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FPMainScreen: UIView {
+    
+    var myoImage: UIImageView
+    
+    
+    
+}
