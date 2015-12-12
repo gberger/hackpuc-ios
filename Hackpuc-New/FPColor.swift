0ca9934ad9978d23fb83f202f9e8aaac3b954eb8
TauanBinato <tauanflores@gmail.com>
2015-12-12 11:15:21 -0200

    FPColor criado

---
+++ Hackpuc-New/FPColor.swift
@@ -0,0 +1,36 @@
+//
+//  FPColor.swift
+//  Hackpuc-New
+//
+//  Created by Tauan Flores on 12/12/15.
+//  Copyright © 2015 Grupo13. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CVColor: NSObject {
+    
+    class func lightGreenColor() -> UIColor {
+        
+        return UIColor(red: 127/255.0, green: 255/255.0, blue: 123/255.0, alpha: 1)
+        
+    }
+    
+    class func blueColor() -> UIColor {
+        
+        return UIColor(red: 122/255.0, green: 211/255.0, blue: 251/255.0, alpha: 1)
+        
+    }
+    
+    class func redColor() -> UIColor {
+        
+        return UIColor(red: 250/255.0, green: 114/255.0, blue: 108/255.0, alpha: 1)
+    }
+    
+    class func whiteColor() -> UIColor {
+        
+        return UIColor(red: 239/255.0, green: 239/255.0, blue: 239/255.0, alpha: 1)
+        
+}
+}
