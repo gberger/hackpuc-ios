8122687b15e32413e8c53c04ba511b1047d41ddc
TauanBinato <tauanflores@gmail.com>
2015-12-12 21:16:01 -0200

    screen msgView desenhada

--- Hackpuc-New/AppDelegate.swift
+++ Hackpuc-New/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        let view = ContactsPresenter()
+        let view = MsgPresenter()
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         self.window?.rootViewController = view
         self.window?.makeKeyAndVisible()
