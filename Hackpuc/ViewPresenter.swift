e9f2d4b3c0e7891209aa79c962083af3a1cef66d
KoJoVe <JoaoVicente@MacBook-Pro-de-Joao-2.local>
2015-12-11 23:55:23 -0200

    Project Initial Structure

---
+++ Hackpuc/ViewPresenter.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Hackpuc
+//
+//  Created by Joao Nassar Galante Guedes on 11/12/15.
+//  Copyright © 2015 Grupo13. All rights reserved.
+//
+
+import UIKit
+
+class ViewPresenter: UIViewController {
+    
+    var myView: UIView {
+        get {
+            return self.view as UIView
+        }
+    }
+
+    override func viewDidLoad() {
+        
+        self.view = UIView()
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
