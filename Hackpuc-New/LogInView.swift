dc821a36af378505614024f91d89cbdc53da8639
TauanBinato <tauanflores@gmail.com>
2015-12-12 13:03:57 -0200

    Mexendo Login

--- Hackpuc-New/LogInView.swift
+++ Hackpuc-New/LogInView.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class LogInView: UIView {
     
+    
+    
     override init(frame: CGRect) {
         
         super.init(frame: frame)
@@ -23,11 +25,32 @@ class LogInView: UIView {
     func setup() {
         
         //Background
-        
         let bgView = UIImageView(image: UIImage(named: "Background.png"))
         bgView.frame = CGRectMake(0, 0, FP.mW(), FP.mH())
         
+        //Delcarações
+        var lPar1: UILabel?
+        
+        // Units
+        let yOffset:        CGFloat = 50
+        let mW:      CGFloat = FP.mW()
+        let mH:     CGFloat = FP.mH() - yOffset
+        let cRa:   CGFloat = FP.cRa()
+        let eleW:  CGFloat = FP.round(382 * FP.wP())
+        let eleBor: CGFloat = FP.round((mW - eleW)/2)
+        
+        
         //Aqui se definem as medidas e as posições dos componentes (Note que os nomes dos componentes são bem pequenos, abreviados)
+        
+        let lPar1Tx:String = "Olá , antes de fazer a conecao com seu dispositivo vamos configurar os alertas.."
+        
+        
+        //Label Parte 1
+        
+        let lP1W: CGFloat = FP.round(eleW - 2*eleBor)
+        let lP1H: CGFloat = FP.round(mH - mW - 6*eleBor)
+        let lP1X: CGFloat = 2*eleBor
+        let lP1Y: CGFloat = 10*eleBor
         
         //Botao de Continuar
         let bConW: CGFloat = FP.wP() * 281
@@ -43,18 +66,32 @@ class LogInView: UIView {
         
         //Aqui se criam os componentes
         
+        //Label Parte 1
+        lPar1 = UILabel(frame: CGRectMake(lP1X,lP1Y,lP1W,lP1H))
+        lPar1?.text = lPar1Tx
+        lPar1?.font = UIFont(name: FP.fontName(), size: FP.normalFS())
+        lPar1?.textColor = FPColor.wColor()
+        lPar1?.textAlignment = .Natural
+        lPar1?.numberOfLines = 99
+        
+        //Continue Button
         let bCon = UIButton(frame: CGRectMake(bConX, bConY, bConW, bConH))
-        bCon.backgroundColor = UIColor.blueColor()
+        bCon.backgroundColor = FPColor.bColor()
         bCon.setTitle("Continuar", forState: .Normal)
         
+        
+        //Pena
         let pena = UIImageView(image: UIImage(named: "Pena.png"))
         pena.frame = CGRectMake(penaX, penaY, penaW, penaH)
+        //pena.layer.cornerRadius = metadetamanho
+        
         
         //Aqui se adicionam os componentes
         
         self.addSubview(bgView)
         self.addSubview(bCon)
         self.addSubview(pena)
+        self.addSubview(lPar1!)
         
     }
 }
