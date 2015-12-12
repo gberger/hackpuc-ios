3187475b459993640db041c73639e3ef70555a0a
Victor <victoraugustolls@gmail.com>
2015-12-12 16:32:35 -0200

    Realm Model

---
+++ Hackpuc-New/REALMContact.swift
@@ -0,0 +1,16 @@
+//
+//  REALMContact.swift
+//  Hackpuc-New
+//
+//  Created by Victor Souza on 12/12/15.
+//  Copyright © 2015 Grupo13. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class REALMContact {
+    
+    dynamic var name = ""
+    dynamic var phone = ""
+}
